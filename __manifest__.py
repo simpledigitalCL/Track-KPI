1560135da9bb033c5ebcc4a46bb263fb4c97b295
Simpledigital <admin@simpledigital.cl>
2026-04-10 13:45:30 -0400

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,8 +1,9 @@
 {
-    'name': 'Track KPI - Ventas y Descuentos',
+    'name': 'Track KPI - Mejora tu repotería',
     'version': '1.0',
     'summary': 'Análisis de descuentos y KPIs comerciales',
     'author': 'Simpledigital',
+    'category': 'Sales',
     'depends': ['account', 'sale'],
     'data': [
         'views/invoice_report_views.xml',
